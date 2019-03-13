07dacf0031c056f4299bb304d028c80e40087d72
Zoe <zoe.gelech@gmail.com>
2019-03-13 10:34:04 -0700

    first commit

---
+++ ageFunction.js
@@ -0,0 +1,13 @@
+//Create a function that take in a name, year of birth and current year
+// create logical equation to get their total age
+// return their name and how many years old they are 
+// return Ex. "Suzie is 32 years old."
+
+function ageCalculator(name, yearOfBirth, currentYear) {
+
+}
+
+
+console.log(ageCalculator("Suzie", 1983, 2015));
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
